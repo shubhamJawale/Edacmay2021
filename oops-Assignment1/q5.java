1c23ba54b673ae425e6ba58d7e1086dfab5e02c6
shubhamJawale <50077968+shubhamJawale@users.noreply.github.com>
2021-05-19 18:03:56 +0530

    Add files via upload

---
+++ oops-Assignment1/q5.java
@@ -0,0 +1,13 @@
+//q5.java
+/*05.Write a program that takes user’s name as command line argument and prints Welcome <entered user name>.*/
+
+class q5{
+
+ static public void main(String args[])
+ {
+ 	String a = args[0];
+ 	System.out.println("Welcome " +a);
+
+ }
+
+}
